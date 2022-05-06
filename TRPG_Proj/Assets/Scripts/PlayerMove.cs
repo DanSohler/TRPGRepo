618bfd88307964ee439069e67333ad95f1175446
Dan Sohler <daniel.sohler73@gmail.com>
2022-05-06 22:02:13 +1000

    Minor Push

--- TRPG_Proj/Assets/Scripts/PlayerMove.cs
+++ TRPG_Proj/Assets/Scripts/PlayerMove.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    // idea:
+    // Player has to select char, char then moves to a set point on a 'tile' obj
+    
+    public GameObject playerObj;
+
     
 }
